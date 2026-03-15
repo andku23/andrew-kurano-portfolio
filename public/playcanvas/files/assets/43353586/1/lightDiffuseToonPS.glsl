7b5ef107f58ee8a8aa08603ef35d5958332d4e14
AndrewKurano <andrew.kurano@triggerxr.com>
2026-03-14 18:51:56 -0700

    Initialized vue

--- public/playcanvas/files/assets/43353586/1/lightDiffuseToonPS.glsl
+++
@@ -1,8 +0,0 @@
-uniform sampler2D texture_ramp;
-
-
-float getLightDiffuse(vec3 worldNormal, vec3 viewDir, vec3 lightDirNorm, vec3 dLightDirNormW) {
-    float light = max(dot(worldNormal, -lightDirNorm), 0.0);
-    return texture2D(texture_ramp, vec2(max(light, 0.0), 0.0)).g;
-}
-
