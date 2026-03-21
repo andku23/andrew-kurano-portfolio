2bb43ee51e2557359739a77d8c0eb5a3d2496328
AndrewKurano <kuranoandrew@gmail.com>
2026-03-21 01:40:27 -0700

    Added in featured projects section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script setup>
 import NavBar from './components/navbar.vue';
 import About from './components/about.vue';
+import projectList from '@/content/projects.json'
+import ProjectItem from './components/project-item.vue';
+
+const pipeIndex = (index) => {
+  return index;
+}
+
 </script>
 
 <template>
   <div class = "main-page">
     <NavBar/>
     <About/>
+    <ProjectItem v-for = "(projectData, index) in projectList" :align-left = "pipeIndex(index)%2==0" :project-data="projectData" />
   </div>
 </template>
 
-<style scoped lang='scss'>
+<style lang='scss'>
 @use "./sass/global-classes";
 
 .main-page{
@@ -18,6 +26,10 @@ import About from './components/about.vue';
   width: 100vw;
   height: auto;
   top: 0;
+}
+
+body{
+  margin: 0px;
 }
 
 </style>
--- src/components/project-item.vue
+++ src/components/project-item.vue
@@ -1,18 +1,77 @@
 <script setup>
+
+const props = defineProps({
+  projectData: Object,
+  alignLeft: Boolean
+});
+
 </script>
 
 <template>
-<div class = "project-item">
+<div class = "project-item" :class = "{'align-left': alignLeft, 'align-right': !alignLeft}" :style = "{backgroundColor: projectData?.bgcolor, color: projectData?.textcolor}">
+    <img class = "project-image" :src = "projectData?.image">
+    <div class = "info-section">
+        <h1>{{projectData?.title}}</h1>
+        <p>{{projectData?.description}}</p>
+    </div>
 </div>
 </template>
 
 <style lang="scss" scoped>
+@use "@/sass/fonts.scss";
+@use "@/sass/constants.scss";
+
+.align-left {
+    flex-direction: row;
+}
+
+.align-right {
+    flex-direction: row-reverse;
+}
+
 .project-item{
+    position: relative;
     width: 100%;
     box-sizing: border-box;
 
-    flex: display;
-    flex-direction: row;
+    display: flex;
     justify-content: center;
+    align-items: center;
+
+    padding: 30px;
+    font-size: 18px;
+
+    @media only screen and (max-width: $tablet) {
+      flex-direction: column !important;
+    }
+
+    .project-image{
+        position: relative;
+        width: vw(700);
+        border-radius: 5px;
+
+        @media only screen and (max-width: $tablet) {
+            width: 80%;
+        }
+    }
+
+    .info-section{
+        position: relative;
+        flex-grow: 1;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+
+        margin-left: 20px;
+        margin-right: 20px;
+
+        @media only screen and (max-width: $tablet) {
+            width: 80%;
+            margin-top: 20px;
+        }
+        
+    }
 }
 </style>
